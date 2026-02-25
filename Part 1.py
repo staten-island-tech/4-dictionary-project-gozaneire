af1aaa8c16eeefe457b37f7bca27545bc6f4378e
ry <yeryanyin@gmail.com>
2026-02-25 09:34:35 -0500

    Update Part 1.py

--- Part 1.py
+++ Part 1.py
@@ -1,2 +1,3 @@
-dictionary = ("MW Pocket","MW","Random House","Collins","Oxford","Cambridge","MW American")
-order_request = input("What do you want to buy?",print[dictionary])
+dictionary = print("MW Pocket","MW","Random House","Collins","Oxford","Cambridge","MW American")
+dictionary
+order_request = input("What do you want to buy?")
