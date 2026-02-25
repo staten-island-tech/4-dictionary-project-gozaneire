85387fe8225c1f9427b662a359de4b6bd22293e7
ry <yeryanyin@gmail.com>
2026-02-25 09:47:15 -0500

    Update Part 1.py

--- Part 1.py
+++ Part 1.py
@@ -1,3 +1,16 @@
-dictionary = print("MW Pocket","MW","Random House","Collins","Oxford","Cambridge","MW American")
-dictionary
+MW_Pocket = {
+    "Name": "MW Pocket",
+    "Description": "Small dictionary that can fit in your hand or pocket",
+    "Price": 6.00
+}
+
+MW = {
+    "Name": "Merriam-Webster",
+    "Description": "An complete dictionary made by the most prestigeous company in dictionaries.",
+    "Price": 120.00
+}
+
+
+
+print("MWPocket",[MW],"Random House","Collins","Oxford","Cambridge","MW American")
 order_request = input("What do you want to buy?")
