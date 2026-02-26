1c26b122db9adf03cd26a7a84a70f7ff29028d93
ry <yeryanyin@gmail.com>
2026-02-26 09:22:47 -0500

    A WHOLE LOT OF CODE ADDED

--- Part 1.py
+++ Part 1.py
@@ -4,13 +4,56 @@ MW_Pocket = {
     "Price": 6.00
 }
 
-MW = {
+Merriam_Webster = {
     "Name": "Merriam-Webster",
-    "Description": "An complete dictionary made by the most prestigeous company in dictionaries.",
+    "Description": "A complete dictionary made by the most prestigeous company in dictionaries.",
     "Price": 120.00
 }
 
+Random_House = {
+    "Name": "Random House Webster",
+    "Description": "A cheaper version of Merriam-Webster's dictionary but not as prestigious.",
+    "Price": 10.00
+}
+
+Collins = {
+    "Name": "Collins",
+    "Description": "A dictionary on paperback for English or French and also thesaurus.",
+    "Price": 35.00
+}
+
+Oxford = {
+    "Name": "Oxford",
+    "Description": "A dictionary for British English and named after Oxford University.",
+    "Price": 15.00
+}
+
+Cambridge = {
+    "Name": "Cambridge",
+    "Description": "A dictionary for British English and named after Cambridge University.",
+    "Price": 40.00
+}
+
+MW_Collegiate = {
+    "Name": "Merriam-Webster Collegiate",
+    "Description": "College dictionary by Merriam-Webster.",
+    "Price": 24.95
+}
 
 
-print("MWPocket",[MW],"Random House","Collins","Oxford","Cambridge","MW American")
-order_request = input("What do you want to buy?")
+bookdictionary = ["MW Pocket","Merriam-Webster","Random House","Collins","Oxford","Cambridge","MW Collegiate"]
+print(*bookdictionary, sep="\n")
+order_request = input("Which dictionary do you want to buy?")
+
+x = bookdictionary.index([order_request])
+print(x)
+
+
+"def cost(p,t)"
+"return(p*t)"
+
+"p = price"
+"t = 8.875"
+
+"finalprice = cost(p,t)"
+"print[finalprice]"
