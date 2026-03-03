3c35d5dcb6af8d9d427a661142f79ee81051d400
ry <yeryanyin@gmail.com>
2026-03-03 08:46:12 -0500

    Update Assessment Practice.py

---
+++ Assessment Practice.py
@@ -0,0 +1,12 @@
+#The input will contain the number followed by lines of text, where each line has at least one character and no more than characters.
+
+N = 1
+thelines = input("Insert Line here")
+
+def englishorfrenchsolver(thelines):
+    tcount = 0
+    scount = 0
+    for i in range(N):
+        if (thelines[i] == "t") or (thelines[i] == "T")
+            tcount =+ 1
+        if (thelines[i] == "s")
